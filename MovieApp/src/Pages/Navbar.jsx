11cacf428da5ad34e5cd152911f3bbedd3d54b9c
Vithurshan2002 <sangervithu2002@gmail.com>
2025-05-11 12:58:33 +0530

    update

--- MovieApp/src/Pages/Navbar.jsx
+++ MovieApp/src/Pages/Navbar.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="bg-blue-500 text-white flex justify-between p-4 items-center">
-      <div className="flex space-x-8 items-center">
-        <h1 className="text-black font-extrabold text-2xl ">MovieHunt</h1>
-        <ul className="flex space-x-5 cursor-pointer">
+    <div className="bg-blue-500 text-white flex md:flex-row flex-col justify-between p-4 items-center">
+      <div className="flex md:flex-row flex-col md:space-x-8 items-center">
+        <h1 className="text-black font-extrabold md:text-2xl text-4xl ">MovieHunt</h1>
+        <ul className="flex md:space-x-5 space-x-10 mt-4 md:mt-0 cursor-pointer">
           <li className="cursor-pointer font-semibold hover:font-bold hover:underline underline-offset-4  transition-all">Home</li>
           <li className="cursor-pointer font-semibold hover:font-bold  hover:underline underline-offset-4 transition-all">Top Rates</li>
           <li className="cursor-pointer font-semibold hover:font-bold   hover:underline underline-offset-4 transition-all">Popular</li>
@@ -13,11 +13,11 @@ const Navbar = () => {
         </ul>
       </div>
 
-      <div>
+      <div className="mt-3 md:mt-0">
         <input
           type="text"
           placeholder="Search"
-          className="bg-white text-black px-1  outline-none"
+          className="bg-white text-black px-1  outline-none font-serif" 
         />
         <button className="text-xs bg-blue-400 p-1 rounded-xl cursor-pointer hover:bg-blue-300 font-semibold active:bg-green-700">
           Search
